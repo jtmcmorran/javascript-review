c481f6cb78d555cbae4610a7550c669b612b399c
James McMorran <jtmcmorran@gmail.com>
2021-10-15 11:50:23 -0400

    datatype refresher complete

---
+++ answers.js
@@ -0,0 +1,27 @@
+//1: lightswitch = boolean
+const lightswitch = true;
+//2: email address = string
+const email = "jtmcmorran@gmail.com"
+//3: spaceship = object
+const spaceship = {
+  hull:10,
+  blasters:2,
+  tractorBeam:true,
+  warpDrive:true
+}
+//4: list of student names = array of strings
+const names = ["Jamie", "Jason H", "Jason T", "Jason C"]
+//5: list of student names with locations = array of objects
+const names2 = [
+  {name:"jamie", location:'Maryland'},
+  {name:"jasonH", location: 'insert location here'},
+  {name:"jasonT", location:'somewhere'},
+  {name:"jasonC", location:'a cool place'}
+]
+//6:  list of names with locations and favorite shows = array of objects
+const names3 = [
+  {name:'jamie', location:'Maryland', favoriteShow:'Over the Garden Wall'},
+  {name:'jasonH', location:'insert location here',favoriteShow:'arrow'},
+  {name:'jasonT', location:'somewhere',favoriteShow:'supergirl'},
+  {name:'jasonC', location:'a cool place',favoriteShow:'titans'}
+]
